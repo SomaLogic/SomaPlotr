99672967d4b05cc0888f6961d2fbc4bc9da949a4
Amanda Hiser <ahiser@somalogic.com>
2023-11-21 09:07:27 -0800

    Modify plotLongitudinal() to take quoted arguments
    - modified to take both quoted and unquoted arguments
    - this places plotLongitudinal() in alignment with other SomaPlotr
      functions that can accept ggplot2-style unquoted variable names as
      arguments
    - added example to function documentation
    - added unit tests to evaluate quoted/unquoted function argument
      capabilities

--- R/plotLongitudinal.R
+++ R/plotLongitudinal.R
@@ -17,15 +17,15 @@
 #' @inheritParams boxplotBeeswarm
 #' @param data A data frame containing RFU values to plot, typically
 #'   a `soma_adat` class object.
-#' @param y An un-quoted variable name in `data` for the column containing
-#'   the the y-axis (i.e. typically the "Response" variable).
-#' @param time An un-quoted variable name in `data` for the column containing
-#'   x-axis (i.e. typically the "time" variable).
-#' @param id An un-quoted variable name in `data` for the column containing
-#'   the subject IDs (i.e. how the samples should be connected).
-#' @param color An un-quoted variable name in `data` for the column
-#'   indicating how to color/group the lines. Default is by `Sex`.
-#'   If _no grouping_ is desired, simply create a dummy variable and pass that
+#' @param y A quoted or unquoted variable name in `data` for the column
+#'   containing the y-axis (i.e. typically the "Response" variable).
+#' @param time A quoted or unquoted variable name in `data` for the column
+#'   containing the x-axis (i.e. typically the "time" variable).
+#' @param id A quoted or unquoted variable name in `data` for the column
+#'   containing the subject IDs (i.e. how the samples should be connected).
+#' @param color A quoted or unquoted variable name in `data` for the column
+#'   indicating how to color/group the lines. Default is by `Sex`. If
+#'   _no grouping_ is desired, simply create a dummy variable and pass that
 #'   column. See example below.
 #' @param summary.line A function describing how to summarize the lines.
 #'   Typically [mean()] or [median()]. Set to `NULL` to suppress summary lines.
@@ -49,58 +49,58 @@
 #' df$TimePoint <- factor(df$TimePoint, levels = c("baseline", "6 months",
 #'                                                 "12 months", "24 months"))
 #'
-#'
-#' new <- df |>
-#'   dplyr::mutate(
-#'     id = rep(1:10, times = 4),
-#'     time = dplyr::case_when(     # convert TimePoint -> numeric; distances for plotting
-#'       TimePoint == "baseline" ~ 0,
-#'       TimePoint == "6 months" ~ 6,
-#'       TimePoint == "12 months" ~ 12,
-#'       TimePoint == "24 months" ~ 24),
-#'     Sample = dplyr::case_when(  # recode Sample to correlate with IDs
-#'       id <= 4 ~ "small",
-#'       id > 7  ~ "large",
-#'       TRUE    ~ "medium")
-#'   )
+#' new <- df |>  # Map 'TimePoint' var (character) to a numeric
+#'  dplyr::left_join(data.frame(TimePoint = c("baseline", "6 months", "12 months", "24 months"),
+#'                              time      = c(0,           6,          12,          24)
+#'                              ),
+#'                   by = "TimePoint") |>
+#'  dplyr::mutate(id     = rep(1:10, times = 4),
+#'                Sample = dplyr::case_when(id <= 4 ~ "small", # Recode Sample to correlate with IDs
+#'                                          id > 7  ~ "large",
+#'                                          TRUE    ~ "medium"))
 #'
 #' # No title; no x-axis label; nothing fancy
-#' plotLongitudinal(new, y = seq.1234.56, time = TimePoint, id = id, color = Sample)
+#' plotLongitudinal(new, y = "seq.1234.56", time = "TimePoint", id = "id", color = "Sample")
 #'
-#' # Use 'time' variable for proper spacing along x-axis
-#' plotLongitudinal(new, y = seq.1234.56, id = id, time = time, color = Sample)
+#' # Color lines by the 'Sample' variable, using the 'time' numeric x variable
+#' plotLongitudinal(new, y = "seq.1234.56", time = "time", id = "id",
+#'                  color = "Sample")
 #'
-#' # Use `mean` as the summary lines; compared to `median` default
-#' plotLongitudinal(new, y = seq.1234.56, id = id, time = time, color = Sample,
-#'                  summary.line = mean)
+#' # Use unquoted variable names
+#' plotLongitudinal(new, y = seq.1234.56, time = time, id = id,
+#'                  color = Sample)
+#'
+#' # Use `mean` as the summary lines
+#' plotLongitudinal(new, y = "seq.1234.56", id = "id", time = "time",
+#'                  color = "Sample", summary.line = mean)
 #'
 #' # Suppress summary line with `NULL`
-#' plotLongitudinal(new, y = seq.1234.56, id = id, time = time, color = Pop,
+#' plotLongitudinal(new, y = "seq.1234.56", id = "id", time = "time", color = "Sample",
 #'                  summary.line = NULL, main = "No Summary Lines")
 #'
-#' # Boxplots by time point; `time` must be a factor (TimePoint)
-#' plotLongitudinal(new, seq.1234.56, id = id, time = TimePoint,
-#'                  color = Sample, add.box = TRUE, summary.line = NULL,
+#' # Boxplots by time point; `time` variable (TimePoint) must be a factor
+#' plotLongitudinal(new, "seq.1234.56", id = "id", time = "TimePoint",
+#'                  color = "Sample", add.box = TRUE, summary.line = NULL,
 #'                  main = "With Time Point Boxplots")
 #'
 #' # Suppress group-wise colors
 #' # by setting up `dummy` column
 #' new$dummy <- "A"     # call it anything
-#' plotLongitudinal(new, seq.1234.56, id = id, time = TimePoint,
-#'                  color = dummy, add.box = TRUE, summary.line = NULL,
+#' plotLongitudinal(new, "seq.1234.56", id = "id", time = "TimePoint",
+#'                  color = "dummy", add.box = TRUE, summary.line = NULL,
 #'                  main = "Boxplots | Suppress Group Color") +
 #'   ggplot2::theme(legend.pos = "none")
 #'
-#' p <- plotLongitudinal(new, y = seq.1234.56, id = id, time = time,
-#'                       color = Sample, summary.line = mean)
-#'
 #' # Facet by `Sample`
-#' p + ggplot2::facet_wrap(rlang::sym("Sample")) +
+#' p <- plotLongitudinal(new, y = "seq.1234.56", id = "id", time = "time",
+#'                       color = "Sample", summary.line = mean)
+#'
+#' p + ggplot2::facet_wrap(~Sample) +
 #'   ggplot2::ggtitle("Facet by `Sample`")
 #'
 #' @importFrom dplyr select arrange group_by summarise
 #' @importFrom ggplot2 ggplot labs aes geom_boxplot geom_line geom_point
-#' @importFrom rlang enquo quo_name !! !!!
+#' @importFrom rlang enquo quo_is_null ensym get_expr !! !!!
 #' @importFrom tidyr pivot_longer
 #' @importFrom SomaDataIO is.apt
 #' @export
@@ -111,20 +111,29 @@ plotLongitudinal <- function(data, y, time, id, color = Sex,
                              x.lab = NULL, add.box = FALSE) {
 
   # Set up quosures
-  id    <- enquo(id)
-  y     <- enquo(y)
+  id    <- ensym(id)
+  y     <- ensym(y)
+  time  <- ensym(time)
   color <- enquo(color)
-  time  <- enquo(time)
-  keys  <- c(y, time, id, color)
 
-  # do check for is.apt() on `y`
-  if ( missing(y.lab) && !is.apt(quo_name(y)) ) {
-    y.lab <- quo_name(y)
+  if ( quo_is_null(color) ) { # Prevents forced evaluation of color
+    data$.plot_group_var <- ""
+    .plot_group_var <- ".plot_group_var"
+    keys  <- c(y, time, id, ensym(.plot_group_var))
+  } else {
+    .plot_group_var <- ensym(color)
+    data$.plot_group_var <- data[[get_expr(color)]]
+    keys  <- c(y, time, id, .plot_group_var)
+  }
+
+  # Perform check for is.apt() on `y`
+  if ( missing(y.lab) && !is.apt(y) ) {
+    y.lab <- y
   }
 
   pdata <- data |>
-    dplyr::select(!!! keys) |>
-    pivot_longer(cols = !!y, names_to = "key", values_to = "value") |>
+    dplyr::select(!!!keys) |>
+    pivot_longer(cols = y, names_to = "key", values_to = "value") |>
     arrange(!!id, !!time)
 
   p <- pdata |>
@@ -132,35 +141,39 @@ plotLongitudinal <- function(data, y, time, id, color = Sex,
     geom_line(aes(y      = value,
                   x      = !!time,
                   group  = !!id,
-                  colour = !!color)) +
+                  colour = !!.plot_group_var)) +
     geom_point(aes(y     = value,
                    x     = !!time,
-                   color = !!color),
+                   color = !!.plot_group_var),
                alpha = 0.5, size = size) +
     labs(x = x.lab, title = main, y = y.lab) +
     scale_color_soma() +
     theme_soma() +
     NULL
 
-  # group data summary
+  # Group data summary
   if ( !is.null(summary.line) ) {
     gd <- data |>
       dplyr::select(!!!keys) |>
-      group_by(!!color, !!time) |>
+      group_by(!!.plot_group_var, !!time) |>
       summarise(group_mean = summary.line(!!y))
     p <- p +
       geom_line(data = gd,
                 aes(x      = !!time,
                     y      = group_mean,
-                    group  = !!color,
-                    colour = !!color),
+                    group  = !!.plot_group_var,
+                    colour = !!.plot_group_var),
                 linewidth = 1, linetype = "dashed")
   }
 
   if ( add.box ) {
     p <- p +
-      geom_boxplot(aes(x = !!time, y = value),
+      geom_boxplot(aes(x = .data[[time]], y = value),
                    fill = NA, colour = "grey")
+  }
+
+  if ( quo_is_null(color)) {        # Remove legend when no color/grouping
+    p <- p + guides(color = "none") # var is specified
   }
 
   p
